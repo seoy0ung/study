87f1be8c348a31875e28a09e95013f16e7889c46
DESKTOP-URKE2VL\SSAFY <sy2000m@naver.com>
2026-01-09 16:02:53 +0900

    리액트 ~8강

---
+++ onebite-react/section01/chapter05.js
@@ -0,0 +1,39 @@
+// 1. Number Type
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+console.log(num1 + num2);
+console.log(num1 / num2);
+
+let inf = Infinity;
+let mInf = -Infinity;
+
+let nan = NaN;
+
+console.log(1 * "hello");
+
+// 2. String Type
+let myName = "임서영";
+let myLocation = "대전";
+let introduce = myName + myLocation;
+
+console.log(introduce);
+
+let introduceText = `${myName}은 ${myLocation}이 거주합니다.`;
+console.log(introduceText);
+
+// 3. Boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+// 4. Null Type (아무것도 없다)
+let empty = null;
+
+// 5. Undefined Type
+let none;
+console.log(none);
+
+// null과 undefined의 차이점
+// 1. undefined는 아무것도 할당하지 않았을 때 자동으로
+// 2. null은 직접 우리가 아무것도 없다고 할당해주는 것
---
+++ onebite-react/section01/chapter07.js
@@ -0,0 +1,27 @@
+// 1. 대입 연산자
+let var1 = 1;
+
+// 2. 산술 연산자
+let num1 = 3 + 2;
+let num2 = 3 % 2;
+// -, *, / 등 있음
+
+// 3. 복합 대입 연산자
+let num7 = 10;
+num7 += 20;
+
+// 4. 증감 연산자
+let num8 = 10;
+num8++; // 해당 라인 끝나고 1 추가(후위 연산)
+++num8; // 해당 라인 시작 전에 1 추가(전위 연산)
+// -- 도 가능
+
+// 5. 논리 연산자
+let or = true || false;
+let and = true && false;
+let not = !true;
+
+// 6. 비교 연산자
+let comp1 = 1 === "1";
+let comp2 = 1 !== 2;
+let comp3 = 2 > 1;
